3cbc397708775da3ab532a24663e72bdcacd19d3
Natasha1705 <Cutebunny1705@gmail.com>
2024-09-17 10:36:07 +0300

    1

---
+++ 2/index.js
@@ -0,0 +1,9 @@
+console.log('test');
+// alert( 'Привет')
+// prompt('Введите число')
+let userName
+userName = 'Natasha'
+// console.log(userName)
+// console.log('=====userName=====',userName );
+const secondName = 'Dima'
+console.log('=====secondName=====',secondName );
